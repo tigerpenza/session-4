ef91824f020adf3880835c8f775f3f2780e5fef6
evgenii <tigerpenza@gmail.com>
2022-09-03 12:50:18 +0300

    рекурсии и двумерные массивы

--- Program.cs
+++ Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿// string [,] table = new string [2, 5];
+// string.Empty;
+
+// // table [0,0] // table [0,1] // table [0,2] // table [0,4]
+// // table [1,0] // table [1,1] // table [1,2] // table [1,4]
+// table [ 1, 2 ] = "слово";
+// for (int rows = 0; rows , 2; Rows++);
+
+
+// рекусия вычисления факторила 
+
+// double Factorial (int n)
+// {
+//     if( n==1) return 1;    // 1! = 1 0!= 1
+//     else return n*Factorial (n-1);
+// }
+// for (int i = 1; i < 40; i++)
+// {
+// Console.WriteLine ($"{i}! = {Factorial (i)} ");
+// }
+
+
+
+
+// f(1) = 1
+// f(2) = 1
+// f(n) = false (n-1) + f (n-2 )
+
+// double Fibonaci (int n)
+// {
+// if ( n== 1 || n ==2) return 1;
+// else return Fibonaci (n-1) + Fibonaci (n -2);
+
+// }
+// for (int i=1; i<50; i++)
+// {
+//     Console.WriteLine($"f({i}) = {Fibonaci (i)}");
+// }
